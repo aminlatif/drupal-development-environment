78b7440ab1b0ee239fcfdcc60c2f8382e1de7d0d
Amin Latifkar <amin.latfkar@gmail.com>
2022-03-25 14:17:57 +0430

    Added loading env.

--- install.sh
+++ install.sh
@@ -2,7 +2,11 @@
 
 git submodule update --init --recursive
 
+source ./extra/load-env-file/load-env-file.sh
+
 if [ ! -f .env ]; then
     echo "Creating .env file"
     cp .env.sample .env
 fi
+
+loadEnvFile ".env"
