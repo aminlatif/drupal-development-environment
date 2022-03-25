5a3455f1027bd5eae78a0ddbd299b019fb0548c1
Amin Latifkar <amin.latifkar@gmail.com>
2022-03-26 01:29:01 +0430

    added installation script.

--- drupal/config/default.install-drupal.sh
+++ drupal/config/default.install-drupal.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-echo "Installing Drupal (version: ${DRUPAL_VERSION})"
+# Profiles: standard, minimal, demo_umami, lightning
+DRUPAL_PROFILE='standard'
+
+echo "Installing Drupal (version: ${DRUPAL_VERSION} - profile: ${DRUPAL_PROFILE})"
 
 bash -c "/uninstall-drupal.sh"
 
 cd /var/www/html
 
 if [ "$DRUPAL_VERSION" == "latest" ]; then
-    composer create-project drupal/recommended-project .
+    composer create-project drupal/recommended-project . --no-interaction
 else
-    composer create-project drupal/recommended-project:${DRUPAL_VERSION} .
+    composer create-project drupal/recommended-project:${DRUPAL_VERSION} . --no-interaction
 fi
 
 cd /var/www/html/web/sites/default && ln -s ../../../../files/public files && ls -lh
@@ -19,6 +22,10 @@ cd /var/www/html/web/themes && ln -s ../../../files/themes/custom custom && ls -
 
 cd /var/www/html
 
-composer require --dev drush/drush
+composer require drush/drush --no-interaction
+composer require --dev drupal/core-dev --with-all-dependencies --no-interaction
 
-./vendor/bin/drush si demo_umami --db-url="$DATABASE_DRIVER://$DATABASE_USERNAME:$DATABASE_PASSWORD@$DATABASE_HOST/$DATABASE_NAME" --db-prefix="$PROJECT_DRUPAL_DATABASE_PREFIX" --account-name="admin" --account-pass="admin" --site-name="$PROJECT_TITLE"
+./vendor/bin/drush si $DRUPAL_PROFILE --db-url="$DATABASE_DRIVER://$DATABASE_USERNAME:$DATABASE_PASSWORD@$DATABASE_HOST/$DATABASE_NAME" --db-prefix="$PROJECT_DRUPAL_DATABASE_PREFIX" --account-name="admin" --account-pass="admin" --site-name="$PROJECT_TITLE"
+
+composer require --dev drupal/devel --no-interaction
+./vendor/bin/drush en devel
